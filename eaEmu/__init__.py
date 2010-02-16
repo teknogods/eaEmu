e7656f1ee966d62c4a87ebbb9dafff862c7baea8
05HgJUuA <o128918@rtrtr.com>
2010-02-15 19:42:21 -0800

    added comments to the logging linkage in __init__, explaining how and why it's there

--- eaEmu/__init__.py
+++ eaEmu/__init__.py
@@ -6,6 +6,11 @@ import logging.config
 
 from twisted.python import log
 
+# This twisted log observer is useful for duping all log messages to the
+# built-in python logging module. This is necessary in WingIDE, where the
+# option to catch logged exceptions can be turned on. Without that option and
+# this LogObs, twisted catches and swallows all deferred exceptions printed on
+# __del__(), making it all but impossible to debug them in Wing.
 class LogObs(log.PythonLoggingObserver):
    def emit(self, eventDict):
       text = log.textFromEventDict(eventDict)
@@ -21,7 +26,9 @@ class LogObs(log.PythonLoggingObserver):
 
 logCfg = 'logging.cfg'
 if os.path.isfile(logCfg):
+   ## load the logging config
    logging.config.fileConfig(logCfg)
+   ## start duping the twisted log lines into python's logging module
    LogObs().start()
 else:
    print('{0} not found -- network traffic logging disabled.'.format(logCfg))
