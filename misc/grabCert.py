ed499cb10f82baa0f2e25aacaef7369bda892bd0
05HgJUuA <o128918@rtrtr.com>
2013-03-09 20:29:34 -0800

    fixed & doced broken grabCerts script

--- misc/grabCert.py
+++ misc/grabCert.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+# Install python-tlslite in debian/ubuntu before using.
+# Inspect the written cert with e.g.:
+#   openssl x509 -noout -text -inform DER -in thecertfile.cer
+from __future__ import print_function
 import tlslite.TLSConnection
 from tlslite.HandshakeSettings import HandshakeSettings
 from socket import *
@@ -12,17 +17,20 @@ def getCert(host, port):
    con = tlslite.TLSConnection.TLSConnection(sock)
 
    con.handshakeClientCert(settings=settings)
-   return ''.join(map(chr, con.session.serverCertChain.x509List[0].writeBytes())))
+   return con.session.serverCertChain.x509List[0].writeBytes().tostring()
 
 def main(argv=None):
    argv = argv or sys.argv
 
    if len(argv) != 3:
-      print('need serv and port as args', file=sys.stderr)
+      print('Need serv and port as args', file=sys.stderr)
       return 1
 
    host, port = argv[1], int(argv[2])
-   open('%s-%s.cer' % (host, port),'wb').write(getCert(host, port))
+   certFile = '{host}-{port}.cer'.format(**locals())
+   data = getCert(host, port)
+   print('Writing cert to: {0}'.format(certFile))
+   open(certFile,'wb').write(data)
 
 if __name__ == '__main__':
    sys.exit(main())
